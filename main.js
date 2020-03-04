8f014b31cc1bd2f0a6d7c3849890adbbad5d20fb
katesivy <58646769+katesivy@users.noreply.github.com>
2020-03-03 21:36:50 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+
+button.addEventListener('click', function);
+
+
+// let requestURL =
+// let request = new XMLHttpRequest();
+// request.open('GET', requestURL);
+// request.responseType = 'json';
+// request.send();
+/*request.onload = function() {
+    const varName = request.response;
+    populateHeader(varName);
+    showHeroes(varName);
+  }*/
